af16b39163cd07f60652214a1653f1161dff5270
Piotrek K <hehlolrofl@gmail.com>
2021-02-13 12:29:45 +0100

    Added contact route + template

--- main.go
+++ main.go
@@ -7,32 +7,36 @@ import (
 )
 
 var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 // w -> writer -> miejsce do którego wypiszemy wynik naszej funkcji
 // wynikiem może być teskt, html, lub inny fomrat danych
 // req -> struct opisuje rządanie do naszego programu
 
 func home(w http.ResponseWriter, req *http.Request) {
+
 	w.Header().Set("Content-Type", "text/html")
 	homeTemplate.Execute(w, nil)
 }
 
-func user(w http.ResponseWriter, req *http.Request) {
+func contact(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "<h1>Hello, my user </h1>")
+	w.Header().Set("Content-Type", "text/html")
+	contactTemplate.Execute(w, nil)
 }
 
 func main() {
 
 	//Pasrowanie szablonów(templaty) html
 	homeTemplate = template.Must(template.ParseFiles("./view/home.html"))
+	contactTemplate = template.Must(template.ParseFiles("./view/contact.html"))
 
 	// mux => HTTP request multiplexer => program który obsługuje  requesty od klientów
 	// dopasowuje url do opowiedniej funkcji
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/contact/", user)
+	mux.HandleFunc("/contact/", contact)
 
 	fmt.Println("Server start with a bump")
 
