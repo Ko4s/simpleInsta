2b17498e1b9eddb6a4258d2c18c756b4d970aa04
Piotrek K <hehlolrofl@gmail.com>
2021-02-13 13:36:35 +0100

    Started to build view struct for handling html/template files

--- main.go
+++ main.go
@@ -8,28 +8,46 @@ import (
 
 var homeTemplate *template.Template
 var contactTemplate *template.Template
+var faqTemplate *template.Template
 
 // w -> writer -> miejsce do którego wypiszemy wynik naszej funkcji
 // wynikiem może być teskt, html, lub inny fomrat danych
 // req -> struct opisuje rządanie do naszego programu
 
 func home(w http.ResponseWriter, req *http.Request) {
-
 	w.Header().Set("Content-Type", "text/html")
-	homeTemplate.Execute(w, nil)
+
+	dumbUser := User{
+		Name:  "Piotrek",
+		Color: "Red",
+		Age:   253623,
+	}
+
+	homeTemplate.ExecuteTemplate(w, "bootstrap", dumbUser)
 }
 
 func contact(w http.ResponseWriter, req *http.Request) {
-
 	w.Header().Set("Content-Type", "text/html")
 	contactTemplate.Execute(w, nil)
+}
+
+func faq(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	faqTemplate.Execute(w, nil)
+}
+
+type User struct {
+	Name  string
+	Color string
+	Age   int
 }
 
 func main() {
 
 	//Pasrowanie szablonów(templaty) html
-	homeTemplate = template.Must(template.ParseFiles("./view/home.html"))
+	homeTemplate = template.Must(template.ParseFiles("./view/layouts/bootstrap.html", "./view/home.html"))
 	contactTemplate = template.Must(template.ParseFiles("./view/contact.html"))
+	faqTemplate = template.Must(template.ParseFiles("./view/faq.html"))
 
 	// mux => HTTP request multiplexer => program który obsługuje  requesty od klientów
 	// dopasowuje url do opowiedniej funkcji
@@ -37,6 +55,7 @@ func main() {
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/contact/", contact)
+	mux.HandleFunc("/faq/", faq)
 
 	fmt.Println("Server start with a bump")
 
